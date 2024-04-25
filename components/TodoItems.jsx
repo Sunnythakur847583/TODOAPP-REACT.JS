5475d06b20241b97f0b1573bb54380d3c4ca36e8
Sunnythakur847583 <167014689+Sunnythakur847583@users.noreply.github.com>
2024-04-25 17:14:01 +0530

    Update TodoItems.jsx

--- components/TodoItems.jsx
+++ components/TodoItems.jsx
@@ -6,6 +6,7 @@ const TodoItems = ({ todoItems, onDeleteClick }) => {
     <div className={styles.itemsContainer}>
       {todoItems.map((item) => (
         <TodoItem
+          key={item.name}
           todoDate={item.dueDate}
           todoName={item.name}
           onDeleteClick={onDeleteClick}
